afffb912f697301626cf4e97563959cc5c81f3cd
Engeline <engie.jp@gmail.com>
2018-11-06 20:00:52 -0500

    JS les until 17

---
+++ 11Incr12Decr.js
@@ -0,0 +1,9 @@
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+var myVar = 11;
+
+// Only change code below this line
+myVar--;
---
+++ 13Decimal14Mult.js
@@ -0,0 +1,7 @@
+var ourDecimal = 5.7;
+
+// Only change code below this line
+
+var myDecimal = 5.7;
+
+var product = 2.0 * 2.5;
