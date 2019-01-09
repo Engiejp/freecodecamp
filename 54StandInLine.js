d05ff08bd8a7228735cb2c8e7af977bdc8d1f312
Engeline <engie.jp@gmail.com>
2019-01-08 19:38:02 -0500

    JS less 54 55

---
+++ 54StandInLine.js
@@ -0,0 +1,14 @@
+function nextInLine(arr, item) {
+    // Your code here
+  var addEnd = arr.push(item);
+  var removeFirst = arr.shift();
+    return removeFirst;  // Change this line
+  }
+  
+  // Test Setup
+  var testArr = [1,2,3,4,5];
+  
+  // Display Code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6)); // Modify this line to test
+  console.log("After: " + JSON.stringify(testArr));
